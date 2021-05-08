7b0a8ea729b2993377dae51fe9a3090502374688
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-05-09 01:59:33 +0500

    install react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Navbar bg="primary" expand="lg">
+        <Navbar.Brand href="#home" className='mainDivHome'>Bootcamp Project 4-B</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
