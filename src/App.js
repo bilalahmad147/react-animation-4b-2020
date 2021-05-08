7e6300fafae55ba36dea43fb79704bc5e5193be5
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-05-09 02:09:39 +0500

    start working on Home component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Navbar, Nav } from "react-bootstrap";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
-      <Navbar bg="primary" expand="lg">
-        <Navbar.Brand href="#home" className='mainDivHome'>Bootcamp Project 4-B</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#link">Contact Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Home />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
 
 function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <Navbar bg="primary" expand="lg">
+        <Navbar.Brand href="#home" className="mainDivHome">
+          Bootcamp Project 4-B
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div>
+        
+      </div>
     </div>
   );
 }
